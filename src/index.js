64777521c75c3f14cf5ffb8c68da42340313e454
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-03-10 03:20:56 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let closedBrackets = [];
+    let bracketType;
+    for (let bracket of str) {
+        bracketsConfig.forEach((el) => {
+            if (el.indexOf(bracket) === 0) {
+                if (bracket !== closedBrackets[closedBrackets.length - 1]) {
+                    closedBrackets.push(el[1]);
+                    bracketType = "opened";
+                } else if (bracket === closedBrackets[closedBrackets.length - 1]) {
+                    bracketType = 'closed';
+                }
+                
+            } else if (el.indexOf(bracket) === 1) {
+                bracketType = "closed";
+            }
+        });
+        if (bracketType === "closed") {
+            if (closedBrackets.length === 0) {
+                return false;
+            } else if (bracket !== closedBrackets.pop()) {
+                return false;
+            }
+        }
+    }
+    return closedBrackets.length === 0;
+};
